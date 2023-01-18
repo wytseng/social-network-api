0aa81463176e20fcb5c8c8907c8741d46a50dce4
Steph T <wytseng@uci.edu>
2023-01-17 18:48:52 -0800

    add and delete friend routes function added

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User, Thought } = require('../models');
 
+// user CRUD methods
+
 router.get('/', async (req, res) => {
   try {
     const userData = await User.find({}).select('-__v');
@@ -14,10 +16,9 @@ router.get('/:id', async (req, res) => {
   try {
     const userData = await User.findById(req.params.id).select('-__v').populate("thoughts friends");
     if (!userData) {
-      res.json("User with given id does not exist.");
-    } else {
-      res.json(userData);
+      res.status(404).json("User with given id does not exist.");
     }
+    res.json(userData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -34,7 +35,10 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
-    await User.findByIdAndUpdate(req.params.id, req.body);
+    const userData = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!userData) {
+      res.status(404).json("User with given id does not exist.");
+    }
     res.sendStatus(200);
   } catch(err) {
     res.status(500).json(err);
@@ -43,11 +47,41 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const userData = await User.findById(req.params.id);
+    const userData = await User.findByIdAndDelete(req.params.id);
+    if (!userData) {
+      res.status(404).json("User with given id does not exist.");
+    }
+
     if (userData.thoughts.length > 0) {
       await Thought.deleteMany({username: userData.username});
     }
-    await User.findByIdAndDelete(req.params.id);
+    res.sendStatus(200);
+  } catch(err) {
+    res.status(400).json(err);
+  }
+});
+
+// Friending 
+
+router.post('/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const userData = await User.findByIdAndUpdate(req.params.userId, { $addToSet: { friends: req.params.friendId }}, { new:true });
+    if (!userData) {
+      res.status(404).json("User with given id does not exist.");
+    }
+    res.sendStatus(200);
+  } catch (err) {
+    res.json(400).json(err);
+  }
+});
+
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const userData = await User.findByIdAndUpdate(req.params.userId, { $pull: { friends:  req.params.friendId }}, { new:true });
+    if (!userData) {
+      res.status(404).json("User with given id does not exist.");
+    }
+  
     res.sendStatus(200);
   } catch(err) {
     res.status(400).json(err);
